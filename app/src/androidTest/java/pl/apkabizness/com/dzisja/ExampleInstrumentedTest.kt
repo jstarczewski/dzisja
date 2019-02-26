2812f831592414097391db4742f0ad545a2e6bbd
jstarczewski <jan.a.starczewski@gmail.com>
2019-02-26 23:07:08 +0100

    Initliaj project

---
+++ app/src/androidTest/java/pl/apkabizness/com/dzisja/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package pl.apkabizness.com.dzisja
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("pl.apkabizness.com.dzisja", appContext.packageName)
+    }
+}
