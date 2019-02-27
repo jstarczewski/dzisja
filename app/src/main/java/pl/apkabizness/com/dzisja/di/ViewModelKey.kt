97fe2c8ee10d83b6544ab440bdaf69f1a5e1c2b1
jstarczewski <jan.a.starczewski@gmail.com>
2019-02-27 17:38:53 +0100

    ViewModelKey added for further use

---
+++ app/src/main/java/pl/apkabizness/com/dzisja/di/ViewModelKey.kt
@@ -0,0 +1,19 @@
+package pl.apkabizness.com.dzisja.di
+
+import android.arch.lifecycle.ViewModel
+import dagger.MapKey
+import kotlin.reflect.KClass
+
+/**
+ * TO-DO read more about annotations in Kotlin
+ * */
+
+@MustBeDocumented
+@Target(
+    AnnotationTarget.FUNCTION,
+    AnnotationTarget.PROPERTY_GETTER,
+    AnnotationTarget.PROPERTY_SETTER
+)
+@Retention(AnnotationRetention.RUNTIME)
+@MapKey
+annotation class ViewModelKey(val value: KClass<out ViewModel>)
