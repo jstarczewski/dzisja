ce9f1cd36a058a414f0ca0bd4e9602e8ddbd10ac
jstarczewski <jan.a.starczewski@gmail.com>
2019-04-23 23:44:55 +0200

    Basic test for chaning feelings fab color

--- app/src/main/java/com/clakestudio/pc/dzisja/ui/main/dayinfo/DayInfoFragment.kt
+++ app/src/main/java/com/clakestudio/pc/dzisja/ui/main/dayinfo/DayInfoFragment.kt
@@ -1,19 +1,26 @@
 package com.clakestudio.pc.dzisja.ui.main.dayinfo
 
+import android.content.res.ColorStateList
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.clakestudio.pc.dzisja.R
 import com.clakestudio.pc.dzisja.util.OpenForTesting
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.android.synthetic.main.fragment_add.*
 import javax.inject.Inject
 
 @OpenForTesting
-class DayInfoFragment : Fragment() {
+class DayInfoFragment : Fragment(), View.OnClickListener {
+
 
     @Inject
     lateinit var viewmodelFactory: ViewModelProvider.Factory
@@ -33,6 +40,20 @@ class DayInfoFragment : Fragment() {
 
         fab_add.setOnClickListener { navController().navigate(R.id.action_dayInfoFragment_to_daysFragment) }
 
+    }
+
+    /**
+     * Min SDK will be changed
+     * */
+
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    override fun onClick(v: View?) {
+        when(v?.id) {
+            R.id.fab_add -> navController().navigate(R.id.action_dayInfoFragment_to_daysFragment)
+            else -> {
+                ViewCompat.setBackgroundTintList(v!!, ColorStateList.valueOf(ContextCompat.getColor(context!!, R.color.colorSecondaryDark)))
+            }
+        }
     }
 
     fun navController() = findNavController()
