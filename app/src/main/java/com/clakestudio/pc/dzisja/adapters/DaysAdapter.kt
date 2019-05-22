59a2862a364d067ad1678f3c8393c67bb9ee7d93
Jan Starczewski <jan.a.starczewski@gmail.com>
2019-05-22 14:00:22 +0200

    Days adapter base implementation

--- app/src/main/java/com/clakestudio/pc/dzisja/adapters/DaysAdapter.kt
+++ app/src/main/java/com/clakestudio/pc/dzisja/adapters/DaysAdapter.kt
@@ -1,40 +1,74 @@
 package com.clakestudio.pc.dzisja.adapters
 
+import android.content.Context
+import android.content.res.ColorStateList
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.clakestudio.pc.dzisja.R
+import com.clakestudio.pc.dzisja.data.Day
 import com.clakestudio.pc.dzisja.databinding.DayBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-class DaysAdapter(private var days: ArrayList<String>,
-                  private val onDayClickCallback: (Unit) -> Unit) : RecyclerView.Adapter<DaysAdapter.DaysViewHolder>() {
+class DaysAdapter(
+    private var days: ArrayList<Day>,
+    private val onDayClickCallback: (Unit) -> Unit
+) : RecyclerView.Adapter<DaysAdapter.DaysViewHolder>() {
 
-    class DaysViewHolder(private val binding: DayBinding) :
+    private lateinit var context: Context
+
+    inner class DaysViewHolder(private val binding: DayBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(text: String) {
-        //     binding.tvTitle.text = text
+        private val fabs: List<FloatingActionButton> =
+            listOf(binding.fabAngry, binding.fabHappy, binding.fabNeutral, binding.fabSad, binding.fabOutline)
+
+        fun bind(textDayInfo: String, textDay: String, emojis: List<Boolean>) {
+            binding.tvDay.text = textDay
+            binding.tvDayInfo.text = textDayInfo
+            for (i in 0 until fabs.size) {
+                if (emojis[i])
+                    changeFabColor(fabs[i])
+            }
+        }
+
+        private fun changeFabColor(fab: FloatingActionButton) {
+            val secondary = ColorStateList.valueOf(ContextCompat.getColor(context, R.color.colorSecondaryDark))
+            val primary = ColorStateList.valueOf(ContextCompat.getColor(context, R.color.colorPrimaryDark))
+            fab.apply {
+                backgroundTintList = if (backgroundTintList!! == secondary) primary else secondary
+            }
         }
     }
 
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DaysViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val binding = com.clakestudio.pc.dzisja.databinding.DayBinding.inflate(inflater, parent, false)
+        val binding = DayBinding.inflate(inflater, parent, false)
+        binding.root.setOnClickListener {
+            onDayClickCallback.invoke(Unit)
+        }
+        context = parent.context
         return DaysViewHolder(binding)
     }
 
     override fun getItemCount(): Int = days.size
 
     override fun onBindViewHolder(holder: DaysViewHolder, position: Int) {
-        holder.bind(days[position])
+        holder.bind(
+            days[position].date,
+            days[position].note,
+            days[position].feelings.split(",").map { it.toBoolean() })
     }
 
 
-    private fun setDays(days: ArrayList<String>) {
+    private fun setDays(days: ArrayList<Day>) {
         this.days = days
         notifyDataSetChanged()
     }
 
-    fun replaceData(days: ArrayList<String>) {
+    fun replaceData(days: ArrayList<Day>) {
         setDays(days)
     }
 
