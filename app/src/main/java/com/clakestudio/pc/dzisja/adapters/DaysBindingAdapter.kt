a335dc3473306f775f78fe0c96f9f1a48a0aae86
Jan Starczewski <jan.a.starczewski@gmail.com>
2019-05-22 14:48:51 +0200

    Example chart showing example data

--- app/src/main/java/com/clakestudio/pc/dzisja/adapters/DaysBindingAdapter.kt
+++ app/src/main/java/com/clakestudio/pc/dzisja/adapters/DaysBindingAdapter.kt
@@ -2,12 +2,13 @@ package com.clakestudio.pc.dzisja.adapters
 
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.clakestudio.pc.dzisja.data.Day
 
 object DaysBindingAdapter {
 
     @JvmStatic
     @BindingAdapter("app:days")
-    fun setDays(recyclerView: RecyclerView, days: ArrayList<String>) {
+    fun setDays(recyclerView: RecyclerView, days: ArrayList<Day>) {
         with(recyclerView.adapter as DaysAdapter) {
             replaceData(days)
         }
